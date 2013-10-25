062c5b529b1c64069b7344a11ea711f09b652e65
gruan <gruan@umail.iu.edu>
2013-10-25 16:32:15 -0400

    in hypervisorSwitch command, add update VM mode operations

--- src/main/java/edu/indiana/d2i/sloan/hyper/SwitchVMCommand.java
+++ src/main/java/edu/indiana/d2i/sloan/hyper/SwitchVMCommand.java
@@ -3,8 +3,10 @@ package edu.indiana.d2i.sloan.hyper;
 import org.apache.log4j.Logger;
 
 import edu.indiana.d2i.sloan.bean.VmInfoBean;
+import edu.indiana.d2i.sloan.db.DBOperations;
 import edu.indiana.d2i.sloan.exception.RetriableException;
 import edu.indiana.d2i.sloan.exception.ScriptCmdErrorException;
+import edu.indiana.d2i.sloan.vm.VMMode;
 import edu.indiana.d2i.sloan.vm.VMState;
 import edu.indiana.d2i.sloan.vm.VMStateManager;
 
@@ -30,13 +32,31 @@ public class SwitchVMCommand extends HypervisorCommand {
 				throw new ScriptCmdErrorException(String.format(
 						"Failed to excute command:\n%s ", resp));
 			}
-			
+
 		} catch (Exception e) {
 			throw new RetriableException(e.getMessage(), e);
 		}
 
+		// update state
 		VMStateManager.getInstance().transitTo(vminfo.getVmid(),
 				vminfo.getVmstate(), VMState.RUNNING);
+
+		// update mode
+
+		assert vminfo.getVmmode().equals(VMMode.SECURE)
+				|| vminfo.getVmmode().equals(VMMode.MAINTENANCE);
+
+		VMMode targetMode = vminfo.getVmmode().equals(VMMode.SECURE)
+				? VMMode.MAINTENANCE
+				: VMMode.SECURE;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format(
+					"Going to update VM mode in DB from %s to %s",
+					vminfo.getVmmode(), targetMode));
+		}
+
+		DBOperations.getInstance().updateVMMode(vminfo.getVmid(), targetMode);
 
 	}
 
@@ -44,6 +64,8 @@ public class SwitchVMCommand extends HypervisorCommand {
 	public void cleanupOnFailed() throws Exception {
 		VMStateManager.getInstance().transitTo(vminfo.getVmid(),
 				vminfo.getVmstate(), VMState.ERROR);
+
+		// TODO: should we also update VM mode to NOT_DEFINED ?
 	}
 
 	@Override
