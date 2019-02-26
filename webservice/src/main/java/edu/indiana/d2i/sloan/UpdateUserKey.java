657651aad17db9f061f73a7d5ef25a422af3fa95
charitha306 <charitha306@gmail.com>
2019-02-26 16:25:07 -0500

    Adding API method to update all non-null public keys of users in all the operational VMs

--- webservice/src/main/java/edu/indiana/d2i/sloan/UpdateUserKey.java
+++ webservice/src/main/java/edu/indiana/d2i/sloan/UpdateUserKey.java
@@ -15,19 +15,15 @@
  ******************************************************************************/
 package edu.indiana.d2i.sloan;
 
-import edu.indiana.d2i.sloan.bean.ErrorBean;
-import edu.indiana.d2i.sloan.bean.QueryVmResponseBean;
-import edu.indiana.d2i.sloan.bean.VmInfoBean;
-import edu.indiana.d2i.sloan.bean.VmStatusBean;
+import edu.indiana.d2i.sloan.bean.*;
 import edu.indiana.d2i.sloan.db.DBOperations;
+import edu.indiana.d2i.sloan.exception.InvalidHostNameException;
 import edu.indiana.d2i.sloan.exception.NoItemIsFoundInDBException;
 import edu.indiana.d2i.sloan.hyper.HypervisorProxy;
 import edu.indiana.d2i.sloan.hyper.QueryVMCommand;
 import edu.indiana.d2i.sloan.hyper.UpdatePublicKeyCommand;
 import edu.indiana.d2i.sloan.utils.RolePermissionUtils;
-import edu.indiana.d2i.sloan.vm.VMMode;
-import edu.indiana.d2i.sloan.vm.VMState;
-import edu.indiana.d2i.sloan.vm.VMStateManager;
+import edu.indiana.d2i.sloan.vm.*;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -46,7 +42,7 @@ public class UpdateUserKey {
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response queryVMs(@FormParam("pubkey") String pubkey,
+	public Response updateUserKey(@FormParam("pubkey") String pubkey,
 			@Context HttpHeaders httpHeaders,
 			@Context HttpServletRequest httpServletRequest) {		
 		String userName = httpServletRequest.getHeader(Constants.USER_NAME);
@@ -99,5 +95,57 @@ public class UpdateUserKey {
 			return Response.status(500)
 					.entity(new ErrorBean(500, e.getMessage())).build();
 		}
+	}
+
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUserKeyUtil(@FormParam("vmid") String vmid,
+							  @Context HttpHeaders httpHeaders,
+							  @Context HttpServletRequest httpServletRequest) {
+
+		try {
+			List<VmInfoBean> vmInfoBeans = new ArrayList<VmInfoBean>();
+			if(vmid != null ) {
+				vmInfoBeans.add(DBOperations.getInstance().getAllVmInfoByID(vmid));
+			} else {
+				vmInfoBeans = DBOperations.getInstance().getAllVmInfo();
+			}
+
+			//Update the VMs with the public keys of all users
+			for(VmInfoBean vmInfoBean : vmInfoBeans) {
+				String vmId = vmInfoBean.getVmid();
+				List<VmUserRole> roles = DBOperations.getInstance().getRolesWithVmid(vmId, true);
+				//iterate all the roles
+				for(VmUserRole vmUserRole : roles) {
+					String guid = vmUserRole.getGuid();
+					String pubkey = DBOperations.getInstance().getUserPubKey(guid);
+					//if role has a public key in database
+					if (pubkey != null) {
+						//if user is allowed to update ssh key in VM
+						if (RolePermissionUtils.isPermittedCommand(guid, vmId,
+								RolePermissionUtils.API_CMD.UPDATE_SSH_KEY)) {
+							// add user's public key to VM
+							logger.info("Public key of user with GUID " + guid + " is going to be added to VM " + vmId);
+							HypervisorProxy.getInstance().addCommand(
+									new UpdatePublicKeyCommand(vmInfoBean, guid, guid, pubkey));
+						}
+					}
+				}
+
+			}
+
+			return Response.status(200).build();
+		} catch (NoItemIsFoundInDBException e) {
+			logger.error(e.getMessage(), e);
+			return Response.status(400)
+					.entity(new ErrorBean(400, "VM with " + vmid + " cannot be found")).build();
+		}  catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return Response.status(500)
+					.entity(new ErrorBean(500, e.getMessage())).build();
+		}
+
 	}
 }
