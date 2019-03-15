e664e4f5baf4651f3974c55c97f00b624040f2fa
Samitha Liyanage <samitha.liyanage@gmail.com>
2019-03-15 13:21:33 -0400

    synched with prod stack backend scripts.

--- backend/startvm.sh
+++ backend/startvm.sh
@@ -380,8 +380,12 @@ fi
 # update htrc package
 GMC_PRIVATE_KEY
 logger "$VM_DIR Update htrc package"
-ssh -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY dcuser@$VM_IP_ADDR "/opt/anaconda/bin/pip uninstall -y htrc" > $VM_DIR/uninstall_htrc_out 2>&1
-ssh -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY dcuser@$VM_IP_ADDR "/opt/anaconda/bin/pip install htrc --no-cache" > $VM_DIR/install_htrc_out 2>&1
+scp -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY -r $GUEST_SCRIPTS dcuser@$VM_IP_ADDR:/tmp/ > $VM_DIR/install_python_packages_out 2>&1
+ssh -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY dcuser@$VM_IP_ADDR "/opt/anaconda/bin/pip install --upgrade pip && /opt/anaconda/bin/pip install --upgrade htrc-feature-reader && /opt/anaconda/bin/pip install -U topicexplorer==1.0b222 && /opt/anaconda/bin/pip install --upgrade htrc && /opt/anaconda/bin/python /tmp/guest_scripts/download_nltk_data.py" >> $VM_DIR/install_python_packages_out 2>&1
+
+#ssh -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY dcuser@$VM_IP_ADDR "/opt/anaconda/bin/pip install --upgrade pip && /opt/anaconda/bin/pip install --upgrade htrc-feature-reader && /opt/anaconda/bin/pip install --upgrade topicexplorer && /opt/anaconda/bin/pip install --upgrade htrc && /opt/anaconda/bin/python /tmp/guest_scripts/download_nltk_data.py" >> $VM_DIR/install_python_packages_out 2>&1
+
+ssh -o StrictHostKeyChecking=no  -i $GMC_PRIVATE_KEY dcuser@$VM_IP_ADDR "/bin/rm -r /tmp/guest_scripts" >> $VM_DIR/install_python_packages_out 2>&1
 
 # Return successfully (only reaches here if no errors occur)
 exit 0
