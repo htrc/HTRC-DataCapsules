987a2c16a14a6898ceaf07c23a1afd94649b328d
Samitha Liyanage <shliyana@indiana.edu>
2021-08-26 12:21:52 -0400

    Removed 'htrc download' from JupyterNotebook run.sh script.

--- backend/uploads/jupyter-notebooks/JupyterNotebooks/run.sh
+++ backend/uploads/jupyter-notebooks/JupyterNotebooks/run.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-VOLUME_LIST_PATH="/home/dcuser/HTRC/htrc-id"
+GIT_REPO_PATH="/home/dcuser/HTRC-JupyterNotebooks"
 
-# Get the volume ID list path
-echo Plese enter the absolute path of the volume list\(Default path $VOLUME_LIST_PATH\).
-read idlist_input
-
-if [ -z "$idlist_input" ]
-then
-	echo Input for volume list path is empty. Get the default volume list path $VOLUME_LIST_PATH.
-else
-        VOLUME_LIST_PATH=$idlist_input
-fi
-
-echo Downloading volumes of $VOLUME_LIST_PATH
-htrc download -f $VOLUME_LIST_PATH
-
-cd /home/dcuser/HTRC-JupyterNotebooks
+cd $GIT_REPO_PATH
 export BROWSER=google-chrome
 source activate nb_env
 jupyter notebook
--- backend/uploads/jupyter-notebooks/install.sh
+++ backend/uploads/jupyter-notebooks/install.sh
@@ -3,21 +3,21 @@
 DC_USER=dcuser
 DC_HOME=/home/dcuser
 NB_ENV=nb_env
+INSTALLER_PATH=/tmp/jupyter-notebooks
 
 # Clone HTRC-JupyterNotebooks git repository into $DC_HOME
 git clone https://github.com/htrc/HTRC-JupyterNotebooks.git $DC_HOME/HTRC-JupyterNotebooks
 chown -R $DC_USER:$DC_USER $DC_HOME/HTRC-JupyterNotebooks
 chmod -R 775 $DC_HOME/HTRC-JupyterNotebooks
 
-# Set nb_env Python environment ($NB_ENV.tar.gz exists at https://analytics.hathitrust.org/files/nb_env.tar.gz)
-cp ./$NB_ENV.tar.gz /opt/anaconda/envs
+# Set nb_env Python environment ($NB_ENV.tar.gz exists at https://analytics.hathitrust.org/files/nb_env.tar.gz. Please download $NB_ENV.tar.gz into the host's directory)
+cp $INSTALLER_PATH/$NB_ENV.tar.gz /opt/anaconda/envs
 cd /opt/anaconda/envs
 mkdir -p ./$NB_ENV
 tar -xzf $NB_ENV.tar.gz -C $NB_ENV
-cd -
 
 # Add bash script and Desktop icon
-cp -r JupyterNotebooks /opt/applications
+cp -r $INSTALLER_PATH/JupyterNotebooks /opt/applications
 chown -R $DC_USER:$DC_USER /opt/applications/JupyterNotebooks
 cp /opt/applications/JupyterNotebooks/notebook.desktop $DC_HOME/Desktop
 chown -R $DC_USER:$DC_USER $DC_HOME/Desktop/notebook.desktop
